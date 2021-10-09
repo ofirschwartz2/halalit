6140c9ec88eaa5780571dd38d30d86752c6762d2
Ofir Schwartz <ofirschwartz2@gmail.com>
2021-10-09 19:05:38 +0300

    added gun and first shot

---
+++ Assets/ShotScript.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShotScript : MonoBehaviour
+{
+    public float speed = 10f;
+    public Rigidbody2D rigidbody;
+    void Start()
+    {
+        rigidbody.velocity = transform.up * speed;
+    }
+    void OnTriggerEnter2D(Collider2D hitInfo){
+        Debug.Log(hitInfo.name);
+        Destroy(gameObject);
+    }
+
+}
