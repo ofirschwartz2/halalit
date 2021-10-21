6a9cdf3bb364a49b3f749d42edaf6ce1f1d3cc58
Ofir Schwartz <ofirschwartz2@gmail.com>
2021-10-21 18:56:48 +0300

    some progress

--- Assets/Scripts/gunController.cs
+++ Assets/Scripts/gunController.cs
@@ -6,13 +6,47 @@ public class gunController : MonoBehaviour
 {
     public Transform firePoint;
     public GameObject shotPrefab;
+    public Joystick gunJoystick;
+    public float cooldownTime = 0;
+    public float cooldownInterval = 1;
     void Update()
     {
-        if (Input.GetButtonDown("Fire1")){
+        Debug.Log("ANGLE IS: "+ getAngle());
+        //Debug.Log("HORIZONTAL: " + gunJoystick.Horizontal + ", VERTICAL: " + gunJoystick.Vertical);
+        if (coolDownPassed() && joystickShouldShoot()){
             shoot();
         }
     }
+    float getAngle(){
+        if(gunJoystick.Vertical > 0){
+            if(gunJoystick.Horizontal > 0) {
+                return (Mathf.Asin(gunJoystick.Vertical / gunJoystick.Horizontal) * 180/Mathf.PI);
+            } else{
+                return (180 - (Mathf.Asin(gunJoystick.Vertical / (gunJoystick.Horizontal * -1)) * 180/Mathf.PI));
+            }
+        } else{
+            if(gunJoystick.Horizontal < 0) {
+                return (180 + (Mathf.Asin((gunJoystick.Vertical * -1) / (gunJoystick.Horizontal * -1)) * 180/Mathf.PI));
+            } else{
+                return (360 - (Mathf.Asin((gunJoystick.Vertical * -1) / gunJoystick.Horizontal) * 180/Mathf.PI));
+            }
+        }
+    }
+    bool coolDownPassed(){
+        if(Time.time >= cooldownTime){
+            return true;
+        } 
+        return false;
+    }
+    bool joystickShouldShoot(){
+        //Debug.Log("SHOULDSHOOT: " + Mathf.Sqrt(Mathf.Pow(gunJoystick.Horizontal, 2) + Mathf.Pow(gunJoystick.Vertical, 2)));
+        if (Mathf.Sqrt(Mathf.Pow(gunJoystick.Horizontal, 2) + Mathf.Pow(gunJoystick.Vertical, 2))  > 0.9){
+            return true;
+        }
+        return false;
+    }
     void shoot() {
         Instantiate(shotPrefab, firePoint.position, firePoint.rotation);
+        cooldownTime = Time.time + cooldownInterval;
     }
 }
