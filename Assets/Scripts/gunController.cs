f4262856a6efd8f734e8ed7a0ed3ff3692ab2541
Ofir Schwartz <ofirschwartz2@gmail.com>
2021-10-24 09:41:22 +0300

    fixes after CR

--- Assets/Scripts/gunController.cs
+++ Assets/Scripts/gunController.cs
@@ -4,51 +4,78 @@ using UnityEngine;
 
 public class gunController : MonoBehaviour
 {
+    const float RADIUS = 2.6f;
+    const float SHOOTING_TH = 0.8f;
+    const float COOL_DOWN_INTERVAL = 0.5f;
+    public float cooldownTime = 0;
     public Transform firePoint;
     public GameObject shotPrefab;
     public Joystick gunJoystick;
-    public float cooldownTime = 0;
-    public float cooldownInterval = 0.5f;
-    public static float radius = 2.6f;
-    public float shootingTH = 0.8f;
 
 
     void Update()
     {
-        if (coolDownPassed() && joystickShouldShoot()){
-            shoot();
-        }
-        if(gunJoystickPressed()){
-            gunLocation();
-        }
+        if (CoolDownPassed() && JoystickShooting())
+            Shoot();
+        if(GunJoystickPressed())
+            ChangeGunPosition();
     }
-    bool gunJoystickPressed(){
-        return (gunJoystick.Horizontal != 0 && gunJoystick.Vertical != 0);
+
+    private bool GunJoystickPressed()
+    {
+        return gunJoystick.Horizontal != 0 && gunJoystick.Vertical != 0;
     }
-    void gunLocation(){
-        float x = radius  * Mathf.Cos(getAngle() * Mathf.PI/180);
-        float y = radius  * Mathf.Sin(getAngle() * Mathf.PI/180);
-        // TODO: understand what is 11.05f. I don't understand...
-        firePoint.position = new Vector2(x / 11f * 2.6f, y/ 11f * 2.6f);
-        firePoint.rotation=  Quaternion.AngleAxis(getAngle(), Vector3.forward);
+
+    private void ChangeGunPosition()
+    {
+        float angle = GetAngle();
+        float x = GetXFromAngle(RADIUS, angle);
+        float y = GetYFromAngle(RADIUS, angle);
+        firePoint.position = new Vector2(x, y);
+        firePoint.rotation = Quaternion.AngleAxis(GetAngle(), Vector3.forward);
     }
-    float getAngle(){
+
+    private float GetAngle()
+    {
         Vector2 input = new Vector2(gunJoystick.Horizontal, gunJoystick.Vertical);
         float angle;
-        if(gunJoystick.Vertical < 0)
+        if (gunJoystick.Vertical < 0)
             angle = Vector2.Angle(input, Vector2.left) + 180f;
         else
             angle = Vector2.Angle(input, Vector2.right);
         return angle;
     }
-    bool coolDownPassed(){
-        return (Time.time >= cooldownTime);
+
+    private bool CoolDownPassed()
+    {
+        return Time.time >= cooldownTime;
     }
-    bool joystickShouldShoot(){
-        return (Mathf.Sqrt(Mathf.Pow(gunJoystick.Horizontal, 2) + Mathf.Pow(gunJoystick.Vertical, 2))  > shootingTH);
+
+    private bool JoystickShooting()
+    {
+        return LengthOfLine(gunJoystick.Horizontal, gunJoystick.Vertical) > SHOOTING_TH;
     }
-    void shoot() {
+
+    private void Shoot() 
+    {
         Instantiate(shotPrefab, firePoint.position, firePoint.rotation);
-        cooldownTime = Time.time + cooldownInterval;
+        cooldownTime = Time.time + COOL_DOWN_INTERVAL;
+    }
+
+    private float LengthOfLine(float x, float y)
+    {
+        return Mathf.Sqrt(Mathf.Pow(x, 2) + Mathf.Pow(y, 2));
+    }
+
+    private float GetXFromAngle(float radius, float angle)
+    {
+        // TODO: understand what is 11f. I don't understand...
+        return radius * Mathf.Cos(angle * Mathf.PI/180) / 11f * radius;
+    }
+
+    private float GetYFromAngle(float radius, float angle)
+    {
+        // TODO: understand what is 11f. I don't understand...
+        return radius * Mathf.Sin(angle * Mathf.PI/180) / 11f * radius;
     }
 }
