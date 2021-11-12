a9a5437eecb5a8cc0c3b47531de21a5cf2c9aa17
Ofir Schwartz <ofirschwartz2@gmail.com>
2021-11-12 12:30:07 +0200

    random direction and speed

--- Assets/Scripts/EnemyMovementController.cs
+++ Assets/Scripts/EnemyMovementController.cs
@@ -4,10 +4,17 @@ using UnityEngine;
 
 public class EnemyMovementController : MonoBehaviour
 {
-    public Rigidbody2D _rigidBody;
-    public float xSpeed = -0.001f;
-    public float ySpeed = -0.003f;
+    private Rigidbody2D _rigidBody;
+    private float xSpeed;
+    private float ySpeed;
     
+    void Start()
+    {
+        _rigidBody = GetComponent<Rigidbody2D>(); 
+        xSpeed = Random.Range(-0.001f, 0.001f);
+        ySpeed = Random.Range(-0.002f, 0.002f);
+    }
+
     void Update()
     {
         _rigidBody.velocity = new Vector2(_rigidBody.velocity.x + xSpeed, _rigidBody.velocity.y + ySpeed);
