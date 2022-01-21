656462e1f3dc039e802e3bf382f4a413f3a923fd
Ofir Schwartz <ofirschwartz2@gmail.com>
2022-01-21 17:09:20 +0200

    done rotation

--- Assets/Scripts/AstroidtMovementController.cs
+++ Assets/Scripts/AstroidtMovementController.cs
@@ -11,25 +11,46 @@ public class AstroidtMovementController : MonoBehaviour
     public float MaxXSpeed;
     public float MinYSpeed;
     public float MaxYSpeed;
+    public float MinRotation;
+    public float MaxRotation;
+    private float _rotationSpeed;
 
     void Start()
     {
         if (UseConfigFile)
         {
-            string[] props = {"MinXSpeed", "MaxXSpeed", "MinYSpeed", "MaxYSpeed"};
+            string[] props = {"MinXSpeed", "MaxXSpeed", "MinYSpeed", "MaxYSpeed", "MinRotation", "MaxRotation"};
             Dictionary<string, float> propsFromConfig = ConfigFileReader.GetPropsFromConfig(GetType().Name, props);
             MinXSpeed = propsFromConfig["MinXSpeed"];
             MaxXSpeed = propsFromConfig["MaxXSpeed"];
             MinYSpeed = propsFromConfig["MinYSpeed"];
             MaxYSpeed = propsFromConfig["MaxYSpeed"];
+            MinRotation = propsFromConfig["MinRotation"];
+            MaxRotation = propsFromConfig["MaxRotation"];
         }
-        
-        GetComponent<Rigidbody2D>().velocity = GetAstroidtVelocity(MinXSpeed, MaxXSpeed, MinYSpeed, MaxYSpeed);
+
+        GetComponent<Rigidbody2D>().velocity = GetVelocity(MinXSpeed, MaxXSpeed, MinYSpeed, MaxYSpeed);
+        _rotationSpeed = GetRotationSpeed(MinRotation, MaxRotation);
     }
 
-    private Vector2 GetAstroidtVelocity(float minXSpeed, float maxXSpeed, float minYSpeed, float maxYSpeed)
+    void Update()
+    {
+        UpdateRotation();
+    }
+
+    void UpdateRotation()
+    {
+        transform.Rotate(0, 0, _rotationSpeed, Space.Self);
+    }   
+
+    Vector2 GetVelocity(float minXSpeed, float maxXSpeed, float minYSpeed, float maxYSpeed)
     {
         return new Vector2(UnityEngine.Random.Range(MinXSpeed, MaxXSpeed), UnityEngine.Random.Range(MinYSpeed, MaxYSpeed));
+    }
+
+    private float GetRotationSpeed(float minRotation, float maxRotation)
+    {
+        return UnityEngine.Random.Range(minRotation, maxRotation);
     }
 
     private void OnTriggerEnter2D(Collider2D other)
