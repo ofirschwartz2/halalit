7b5931b69cdaf2771e71b8db56caa55821b07af7
Ofir Schwartz <ofirschwartz2@gmail.com>
2022-01-24 19:09:54 +0200

    struct fot new game object

---
+++ Assets/Enums/NewGameObject.cs
@@ -0,0 +1,9 @@
+﻿namespace Assets.Enums
+{
+    public enum NewGameObject
+    {
+        ENEMY,
+        ASTROID,
+        ITEM
+    }
+}
--- Assets/Scripts/SceneFactory.cs
+++ Assets/Scripts/SceneFactory.cs
@@ -2,24 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using Assets.Common;
 using UnityEngine;
+using Assets.Enums;
+
 
 public class SceneFactory : MonoBehaviour
 {
 
     public bool UseConfigFile;
     public GameObject Background, EnemyPrefab, AstroidPrefab, ItemPrefab;
-    public int MaxNumberOfGameObjectsAllowed, MaxNumberOfEnemiesAllowed, NumberOfEnemies, MaxNumberOfAstroidsAllowed, NumberOfAstroids, MaxNumberOfItemsAllowed, NumberOfItems, GameObjectsOnGameGreedX, GameObjectsOnGameGreedY;
+    public int MaxNumberOfGameObjectsAllowed, MaxNumberOfEnemiesAllowed, NumberOfEnemies, MaxNumberOfAstroidsAllowed, NumberOfAstroids, MaxNumberOfItemsAllowed, NumberOfItems, SlotsOnGameGreedX, SlotsOnGameGreedY;
     
     private float _xGreedSpacing, _yGreedSpacing;
     private Vector3 _bottomLeftPoint; 
     private bool[,] _gameObjectsOnGameGreed; //enemiesOnGameGreed
-
+    //private NewGameObject _newGameObject;
 
     void Start()
     {
         if (UseConfigFile)
         {
-            string[] props = {"MaxNumberOfGameObjectsAllowed", "MaxNumberOfEnemiesAllowed", "NumberOfEnemies", "MaxNumberOfAstroidsAllowed", "NumberOfAstroids", "MaxNumberOfItemsAllowed", "NumberOfItems", "GameObjectsOnGameGreedX", "GameObjectsOnGameGreedY"};
+            string[] props = {"MaxNumberOfGameObjectsAllowed", "MaxNumberOfEnemiesAllowed", "NumberOfEnemies", "MaxNumberOfAstroidsAllowed", "NumberOfAstroids", "MaxNumberOfItemsAllowed", "NumberOfItems", "SlotsOnGameGreedX", "SlotsOnGameGreedY"};
             Dictionary<string, float> propsFromConfig = ConfigFileReader.GetPropsFromConfig(GetType().Name, props);
 
             MaxNumberOfGameObjectsAllowed = (int)propsFromConfig["MaxNumberOfGameObjectsAllowed"];
@@ -31,8 +33,8 @@ public class SceneFactory : MonoBehaviour
             NumberOfAstroids = (int)propsFromConfig["NumberOfAstroids"];
             NumberOfItems = (int)propsFromConfig["NumberOfItems"];
 
-            GameObjectsOnGameGreedX = (int)propsFromConfig["GameObjectsOnGameGreedX"];
-            GameObjectsOnGameGreedY = (int)propsFromConfig["GameObjectsOnGameGreedY"];
+            SlotsOnGameGreedX = (int)propsFromConfig["SlotsOnGameGreedX"];
+            SlotsOnGameGreedY = (int)propsFromConfig["SlotsOnGameGreedY"];
         }
 
         if (TooManyGameObjects()) 
@@ -57,7 +59,7 @@ public class SceneFactory : MonoBehaviour
     {
         for (int i = 0; i < NumberOfEnemies; i++)
         {
-            Vector2 entryPointOnGreed = GetNewEntryPointOnGreed();
+            Vector2 entryPointOnGreed = GetNewEntryPointOnGreed(NewGameObject.ENEMY);
             Instantiate(EnemyPrefab,  GetPointOnGreed(entryPointOnGreed.x,entryPointOnGreed.y), Quaternion.AngleAxis(0, Vector3.forward));
         }
     }
@@ -66,7 +68,7 @@ public class SceneFactory : MonoBehaviour
     {
         for (int i = 0; i < NumberOfAstroids; i++)
         {
-            Vector2 entryPointOnGreed = GetNewEntryPointOnGreed();
+            Vector2 entryPointOnGreed = GetNewEntryPointOnGreed(NewGameObject.ASTROID);
             Instantiate(AstroidPrefab,  GetPointOnGreed(entryPointOnGreed.x,entryPointOnGreed.y), Quaternion.AngleAxis(0, Vector3.forward));
         }
     }
@@ -74,7 +76,7 @@ public class SceneFactory : MonoBehaviour
     {
         for (int i = 0; i < NumberOfItems; i++)
         {
-            Vector2 entryPointOnGreed = GetNewEntryPointOnGreed();
+            Vector2 entryPointOnGreed = GetNewEntryPointOnGreed(NewGameObject.ITEM);
             Instantiate(ItemPrefab,  GetPointOnGreed(entryPointOnGreed.x,entryPointOnGreed.y), Quaternion.AngleAxis(0, Vector3.forward));
         }
     }
@@ -84,28 +86,47 @@ public class SceneFactory : MonoBehaviour
         return _bottomLeftPoint + new Vector3(_xGreedSpacing * xInGameGrid + (_xGreedSpacing / 2), _yGreedSpacing * yInGameGrid + (_yGreedSpacing / 2));
     }
 
-    public Vector2 GetNewEntryPointOnGreed()
+    public Vector2 GetNewEntryPointOnGreed(NewGameObject ngo)
     {
-        Vector2 randPointOnGreed = GetRandomPointOnOneOfTheEdges();
-        while (_gameObjectsOnGameGreed[(int)randPointOnGreed.x, (int)randPointOnGreed.y])
-            randPointOnGreed = GetRandomPointOnOneOfTheEdges();
+        int edgesWidth;
+        Vector2 randPointOnGreed;
+
+        switch (ngo)
+        {
+            case NewGameObject.ENEMY:
+                edgesWidth = 2;
+            break;
+            case NewGameObject.ASTROID:
+                edgesWidth = 1;
+            break;
+            case NewGameObject.ITEM:
+                edgesWidth = SlotsOnGameGreedY / 2 - 2;
+            break;
+            default:
+               throw new System.Exception("GameObject not supported"); 
+        } 
+
+        do
+        {
+            randPointOnGreed = GetRandomPointOnOneOfTheEdges(edgesWidth);
+        } while (_gameObjectsOnGameGreed[(int)randPointOnGreed.x, (int)randPointOnGreed.y]);
         _gameObjectsOnGameGreed[(int)randPointOnGreed.x, (int)randPointOnGreed.y] = true;
         return randPointOnGreed;
     }
 
 
-    public Vector2 GetRandomPointOnOneOfTheEdges()
+    public Vector2 GetRandomPointOnOneOfTheEdges(int edgesWidth)
     {
     switch(Random.Range(0,4))
         {
             case 0:
-                return Utils.GetRandomVector(0, 1, 0, _gameObjectsOnGameGreed.GetLength(0));
+                return Utils.GetRandomVector(0, edgesWidth, 0, _gameObjectsOnGameGreed.GetLength(0));
             case 1:
-                return Utils.GetRandomVector(_gameObjectsOnGameGreed.GetLength(0) - 1, _gameObjectsOnGameGreed.GetLength(0), 0, _gameObjectsOnGameGreed.GetLength(0));
+                return Utils.GetRandomVector(_gameObjectsOnGameGreed.GetLength(0) - edgesWidth, _gameObjectsOnGameGreed.GetLength(0), 0, _gameObjectsOnGameGreed.GetLength(0));
             case 2:
-                return Utils.GetRandomVector(0, _gameObjectsOnGameGreed.GetLength(0), 0, 1);
+                return Utils.GetRandomVector(0, _gameObjectsOnGameGreed.GetLength(0), 0, edgesWidth);
             case 3:
-                return Utils.GetRandomVector(0, _gameObjectsOnGameGreed.GetLength(0), _gameObjectsOnGameGreed.GetLength(0) - 1, _gameObjectsOnGameGreed.GetLength(0));
+                return Utils.GetRandomVector(0, _gameObjectsOnGameGreed.GetLength(0), _gameObjectsOnGameGreed.GetLength(0) - edgesWidth, _gameObjectsOnGameGreed.GetLength(0));
         }
         throw new System.Exception("Not a random between 0 to 3, abort");   
     }
@@ -115,9 +136,9 @@ public class SceneFactory : MonoBehaviour
         var bgSize = Background.GetComponent<Renderer>().bounds.size;
         _bottomLeftPoint = new Vector3(bgSize.x / 2 * (-1), bgSize.y / 2 * (-1));
 
-        _xGreedSpacing = bgSize.x / GameObjectsOnGameGreedX;
-        _yGreedSpacing = bgSize.y / GameObjectsOnGameGreedY;
+        _xGreedSpacing = bgSize.x / SlotsOnGameGreedX;
+        _yGreedSpacing = bgSize.y / SlotsOnGameGreedY;
 
-        _gameObjectsOnGameGreed  = new bool[GameObjectsOnGameGreedX,GameObjectsOnGameGreedY];
+        _gameObjectsOnGameGreed  = new bool[SlotsOnGameGreedX,SlotsOnGameGreedY];
     }
 }
