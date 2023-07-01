10d2f8b536ee5a4ad31908096a75a7813412e5be
Ofir Schwartz <ofirschwartz2@gmail.com>
2023-07-01 11:27:30 +0300

    added most of the logic. still not  moving

--- Assets/Scripts/ZigZagEnemyMovementController.cs
+++ Assets/Scripts/ZigZagEnemyMovementController.cs
@@ -3,20 +3,37 @@ using System.Collections.Generic;
 using Assets.Common;
 using UnityEngine;
 
-public class ZigZagEnemyMovementController : MonoBehaviour 
+public class ZigZagEnemyMovementController : MonoBehaviour
 {
-    
+
+    public float EnemyThrust, SpeedLimit, KnockbackGunMultiplier;
+    public bool UseConfigFile;
+    public int ItemDropRate;
+    private Rigidbody2D _rigidBody;
+    private float _xForce, _yForce;
+    public GameObject ItemPrefab;
+
+
     void Start()
     {
-        _rigidBody = GetComponent<Rigidbody2D>(); 
+        _rigidBody = GetComponent<Rigidbody2D>();
         if (UseConfigFile)
             ConfigureFromFile();
-            
+
+        _xForce = 1;
+        _yForce = 1;
         tag = "ZigZagEnemy";
     }
 
     void Update()
     {
+        if (IsUnderSpeedLimit())
+            _rigidBody.AddForce(new Vector2(_xForce, _yForce));
+    }
+
+    private bool IsUnderSpeedLimit()
+    {
+        return Utils.GetVectorMagnitude(_rigidBody.velocity) < SpeedLimit;
     }
 
     void InnerOnTriggerEnter2D(Collider2D other)
@@ -31,25 +48,25 @@ public class ZigZagEnemyMovementController : MonoBehaviour
             GoInAnotherDirection();
     }
 
-    
+
     private void KillMe(Collider2D other)
     {
-        if(ShouldDropItem())
+        if (ShouldDropItem())
         {
             Vector2 normalizedDifference = (_rigidBody.transform.position - other.transform.position).normalized;
 
-            GameObject item = Instantiate(ItemPrefab,  _rigidBody.transform.position, Quaternion.AngleAxis(0, Vector3.forward));
+            GameObject item = Instantiate(ItemPrefab, _rigidBody.transform.position, Quaternion.AngleAxis(0, Vector3.forward));
             item.SendMessage("StartFromInstantiation", normalizedDifference);
         }
 
         Destroy(gameObject);
     }
-    
+
     private bool ShouldDropItem()
     {
-        return Random.Range(0,100) < ItemDropRate;
+        return Random.Range(0, 100) < ItemDropRate;
     }
-    
+
     private void GoInAnotherDirection()
     {
     }
@@ -63,7 +80,11 @@ public class ZigZagEnemyMovementController : MonoBehaviour
 
     private void ConfigureFromFile()
     {
-        string[] props = {};
+        string[] props = { "SpeedLimit", "EnemyThrust", "KnockbackGunMultiplier", "ItemDropRate" };
         Dictionary<string, float> propsFromConfig = ConfigFileReader.GetPropsFromConfig(GetType().Name, props);
+        SpeedLimit = propsFromConfig["SpeedLimit"];
+        EnemyThrust = propsFromConfig["EnemyThrust"];
+        KnockbackGunMultiplier = propsFromConfig["KnockbackGunMultiplier"];
+        ItemDropRate = (int)propsFromConfig["ItemDropRate"];
     }
 }
