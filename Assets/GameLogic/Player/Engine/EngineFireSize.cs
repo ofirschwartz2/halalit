df4bc292fd7fd9e5d481fbfecb7bd5f5a4e8607e
Ofir Schwartz <ofirschwartz2@gmail.com>
2023-10-19 19:25:43 +0300

    added fire from engine

---
+++ Assets/GameLogic/Player/Engine/EngineFireSize.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System;
+using Assets.Utils;
+
+class EngineFireSize : MonoBehaviour
+{
+    [SerializeField]
+    private Joystick _movementJoystick;
+
+    void Update()
+    {
+        SetFireSize();
+    }
+
+    private void SetFireSize()
+    {
+        float movementForce = MovementJoystickPressed() ? Utils.GetLengthOfLine(Math.Abs(_movementJoystick.Horizontal), Math.Abs(_movementJoystick.Vertical)) : 0;
+        float newLocalPositionX = -movementForce / 2;
+
+        transform.localScale = new Vector3(movementForce, transform.localScale.y);
+        transform.localPosition = new Vector2(newLocalPositionX, transform.localPosition.y);
+    }
+
+    private bool MovementJoystickPressed()
+    {
+        return _movementJoystick.Horizontal != 0 || _movementJoystick.Vertical != 0;
+    }
+}
