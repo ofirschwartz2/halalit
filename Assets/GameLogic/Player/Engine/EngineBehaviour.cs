74c55ce033e70241de7a73fbc175a24cfd8ad893
Ofir Schwartz <ofirschwartz2@gmail.com>
2023-10-20 14:14:19 +0300

    added engine fire behaviour

---
+++ Assets/GameLogic/Player/Engine/EngineBehaviour.cs
@@ -0,0 +1,45 @@
+﻿using Assets.Utils;
+using System;
+using UnityEngine;
+
+class EngineBehaviour : MonoBehaviour
+{
+    [SerializeField]
+    private Joystick _movementJoystick;
+
+    [SerializeField]
+    private float _fireAngle;
+
+    [SerializeField]
+    private float _fireForceMultiplier;
+
+    [SerializeField]
+    private GameObject _engineFirePrefab;
+
+    void Update()
+    {
+        SetFireSize();
+    }
+
+    private void SetFireSize()
+    {
+        if (MovementJoystickPressed()) 
+        {
+            var movementForce = Utils.GetLengthOfLine(Math.Abs(_movementJoystick.Horizontal), Math.Abs(_movementJoystick.Vertical)) * _fireForceMultiplier;
+
+            var fire = Instantiate(_engineFirePrefab, transform.position, GetFireAngle());
+            fire.GetComponent<Rigidbody2D>().AddForce(transform.up * movementForce);
+        }
+    }
+
+    private Quaternion GetFireAngle()
+    {
+        var randomAngle = UnityEngine.Random.Range(-_fireAngle, _fireAngle);
+        return Utils.GetRotationPlusAngle(Quaternion.identity, randomAngle);
+    }
+
+    private bool MovementJoystickPressed()
+    {
+        return _movementJoystick.Horizontal != 0 || _movementJoystick.Vertical != 0;
+    }
+}
---
+++ Assets/GameLogic/Player/Engine/EngineFire.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using Assets.Utils;
+
+class EngineFire : MonoBehaviour
+{
+
+    [SerializeField]
+    private float _lifetime;
+
+    private float _endOfLifeTime;
+
+    void Start()
+    {
+        _endOfLifeTime = Utils.GetEndOfLifeTime(_lifetime);
+    }
+
+    void FixedUpdate()
+    {
+        if (Utils.ShouldDie(_endOfLifeTime))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
