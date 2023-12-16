8780455249d3ec3c74cd52fdea8a840c2154e1bc
Ofir Schwartz <ofirschwartz2@gmail.com>
2023-12-16 19:38:39 +0200

    DONE

---
+++ Assets/GameLogic/Player/Halalit/Engine/EngineFire.cs
@@ -0,0 +1,30 @@
+using Assets.Utils;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class EngineFire : MonoBehaviour
+{
+    [SerializeField]
+    private ParticleSystem _engineFireParticleSystem;
+    [SerializeField]
+    private Joystick _joystick;
+    [SerializeField]
+    private float _engineFireForceMultiplier;
+    [SerializeField]
+    private float _engineFireToSpeedMultiplier;
+
+    void FixedUpdate()
+    {
+        var mainModule = _engineFireParticleSystem.main;
+        var fromSpeed = GetEngineFireForce();
+        mainModule.startSpeed = Random.Range(fromSpeed, fromSpeed * _engineFireToSpeedMultiplier);
+    }
+
+    private float GetEngineFireForce()
+    {
+        return Utils.GetLengthOfLine(_joystick.Horizontal, _joystick.Vertical) * _engineFireForceMultiplier;
+    }
+
+
+
+}
