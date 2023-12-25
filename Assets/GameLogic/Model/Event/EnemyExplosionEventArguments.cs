3fa87c664ae25dae8509d9d461aa531013669e42
Ofir Schwartz <ofirschwartz2@gmail.com>
2023-12-25 18:16:53 +0200

    done with explosion event

---
+++ Assets/GameLogic/Model/Event/EnemyExplosionEventArguments.cs
@@ -0,0 +1,12 @@
+﻿using Assets.Enums;
+using UnityEngine;
+
+public class EnemyExplosionEventArguments : System.EventArgs
+{
+    public Vector2 Position;
+
+    public EnemyExplosionEventArguments(Vector2 position)
+    {
+        Position = position;
+    }
+}
