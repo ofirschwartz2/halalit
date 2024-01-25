3413b4d20c21ee4a4dbd5cfe0e76d9385daec9a1
Ofir Schwartz <ofirschwartz2@gmail.com>
2024-01-25 16:15:39 +0200

    first test

---
+++ Assets/Tests/EditMode/MovementTests.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+public class MovementTests
+{
+    [Test]
+    public void testtttt()
+    {
+        Assert.True(true);
+    }
+}
