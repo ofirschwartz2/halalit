ad8a5e5dd28411b4d6de8ece17a471b7ebdd877d
Ofir Schwartz <ofirschwartz2@gmail.com>
2024-02-04 17:06:31 +0200

    some changes

--- Assets/Tests/PlayMode/Tests/AllInWorld.cs
+++ Assets/Tests/PlayMode/Tests/AllInWorld.cs
@@ -1,9 +1,7 @@
-using System;
+using NUnit.Framework;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using Assets.Enums;
-using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.TestTools;
---
+++ Assets/Tests/PlayMode/Tests/Movement/JoystickDirection.cs
@@ -0,0 +1,54 @@
+using NUnit.Framework;
+using System;
+using System.Collections;
+using System.Runtime.InteropServices;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+using UnityEngine.TestTools;
+using static System.Runtime.CompilerServices.RuntimeHelpers;
+
+
+public class JoystickDirectionTest
+{
+
+    [DllImport("AllAssembly.dll", EntryPoint = "Assets.GameLogic.Utils.GetRandomVector2OnCircle")]
+    public static extern Vector2 GetRandomVector2OnCircle(float radius);
+
+    [DllImport("AllAssembly.dll", EntryPoint = "Assets.GameLogic.Player.Halalit.HalalitMovement")]
+    public static extern void TryMove(float joystickHorizontal, float joystickVertical, float deltaTime);
+
+
+    [SetUp]
+    public void SetUp()
+    {
+        SceneManager.LoadScene("Playground");
+    }
+
+    [UnityTest]
+    public IEnumerator JoystickDirection()
+    {
+        float totalTime = 3f;
+        float elapsedTime = 0f;
+        
+        var randomTouchOnMovementJoystick = GetRandomTouchOnMovementJoystick();
+
+        while (elapsedTime < totalTime)
+        {
+            float deltaTime = Time.deltaTime;
+            elapsedTime += deltaTime;
+            TryMove(randomTouchOnMovementJoystick.x, randomTouchOnMovementJoystick.y, deltaTime);
+
+            yield return null;
+        }
+
+        Assert.IsTrue(true);
+    }
+
+    private Vector2 GetRandomTouchOnMovementJoystick()
+    {
+        GameObject movementJoystick = GameObject.FindGameObjectWithTag("MovementJoystick");
+        RectTransform movementJoystickRectTransform = movementJoystick.GetComponent<RectTransform>();
+        return GetRandomVector2OnCircle(movementJoystickRectTransform.rect.width / 2);
+    }
+}
