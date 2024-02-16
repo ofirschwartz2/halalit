e2e2d0f314567e7f8b7210ca0ec50cffcfd40c9b
Ofir Schwartz <ofirschwartz2@gmail.com>
2024-02-16 18:16:41 +0200

    IsSomewhereOnInternalWorldEdges

--- Assets/Tests/PlayMode/Tests/Attacks/LongRange/BallShot/BallShotTests.cs
+++ Assets/Tests/PlayMode/Tests/Attacks/LongRange/BallShot/BallShotTests.cs
@@ -2,6 +2,7 @@ using Assets.Enums;
 using Assets.Utils;
 using NUnit.Framework;
 using System.Collections;
+using UnityEditor.VersionControl;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.TestTools;
@@ -53,7 +54,20 @@ public class BallShotTests
 
         var shot = GameObject.FindGameObjectWithTag(Tag.SHOT.GetDescription());
 
-        Assert.IsNotNull(shot);
+        Assert.IsNotNull(shot); // There is a shot
+
+        Vector2 lastShotPosition;
+
+        do {
+            lastShotPosition = shot.transform.position;
+            yield return null;
+            shot = GameObject.FindGameObjectWithTag(Tag.SHOT.GetDescription());
+        } while (shot != null);
+
+        Assert.IsTrue(TestUtils.IsSomewhereOnInternalWorldEdges(lastShotPosition));
+
+        // wait until the shot collides with something
+
     }
 
 }
--- Assets/Tests/PlayMode/Tests/TestUtils.cs
+++ Assets/Tests/PlayMode/Tests/TestUtils.cs
@@ -1,5 +1,6 @@
 using Assets.Enums;
 using Assets.Utils;
+using System.Drawing;
 using UnityEngine;
 
 
@@ -29,6 +30,13 @@ internal static class TestUtils
     {
         var weapon = GameObject.FindGameObjectWithTag(Tag.WEAPON.GetDescription());
         return weapon.GetComponent<WeaponMovement>();
+    }
+
+    internal static BoxCollider2D GetInternalWorldBoxCollider2D()
+    {
+        return 
+            GameObject.FindGameObjectWithTag(Tag.INTERNAL_WORLD.GetDescription())
+                .GetComponent<BoxCollider2D>();
     }
     #endregion
 
@@ -60,4 +68,24 @@ internal static class TestUtils
         return randomTouch;
     }
     #endregion
+
+    internal static bool IsSomewhereOnInternalWorldEdges(Vector2 position) 
+    {
+        Vector3 position3 = new Vector3(position.x, position.y, 1); // TODO: why our InternalWorldBoxCollider2DBoxCollider2D Z=1?
+
+        var boxCollider = GetInternalWorldBoxCollider2D();
+        var edgeThreshold = 0.01f;
+        var bounds = boxCollider.bounds;
+
+        var largerBounds = new Bounds(bounds.center, bounds.size * (1 + edgeThreshold));
+        var smallerBounds = new Bounds(bounds.center, bounds.size * (1 - edgeThreshold));
+
+        if (largerBounds.Contains(position3) && !smallerBounds.Contains(position3))
+        {
+            return true;
+        }
+        return false;
+
+    }
+
 }
